1638ebc7e289fe4dda93a32594e5f023dad5b7d7
WenxuanZhang <ll.wxzhang@gmail.com>
2023-09-17 14:14:02 -0500

    hi

--- application.py
+++ application.py
@@ -1,8 +1,13 @@
 from flask import Flask,render_template
+import pandas as pd
+import os
 
 application = Flask(__name__)
+data_path = os.getcwd()+'/data'
+data_file = data_path + '/name.csv'
+data = pd.read_csv(data_file)
 
-name = 'jane'
+name = data['name'][1]
 
 @application.route('/')
 def hello_world():
